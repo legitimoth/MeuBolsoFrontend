28f95828779337ae4d70acb74cdfbd30284cfc8e
Thaynner Bastos <cast.thaynner.utic@sebrae.com.br>
2024-11-15 19:52:39 -0300

    🚧WIP: Criando tela do cartão.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from './modules/navbar/navbar.component';
+import {CartaoComponent} from './modules/cartao/cartao.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CartaoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
