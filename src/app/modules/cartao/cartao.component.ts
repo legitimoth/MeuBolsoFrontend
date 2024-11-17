de273b9633ebf9fd83f0cb0c53b5af09f173ae8c
Thaynner Bastos <cast.thaynner.utic@sebrae.com.br>
2024-11-16 21:52:50 -0300

    🚧WIP: Criando listagem

---
+++ src/app/modules/cartao/cartao.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from '../header/header.component';
+
+@Component({
+  selector: 'app-cartao',
+  standalone: true,
+  imports: [
+    HeaderComponent
+  ],
+  templateUrl: './cartao.component.html',
+  styleUrl: './cartao.component.scss'
+})
+export class CartaoComponent {
+
+}
