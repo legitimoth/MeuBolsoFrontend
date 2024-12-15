313b626eeb45aca67001ace1c5a9687822a41a49
legitimoth <th.arbastos@gmail.com>
2024-12-15 20:23:00 -0300

    ✅Done: Colocando logica de aguardar no guard e tirando do interceptor.

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -1,11 +1,18 @@
 import { CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
-import { firstValueFrom } from 'rxjs';
+import {filter, firstValueFrom, switchMap, take} from 'rxjs';
 
 export const authGuard: CanActivateFn = async (route, state) => {
   const authService = inject(AuthService);
-  const isAuthenticated = await firstValueFrom(authService.isAuthenticated$);
+
+  const isAuthenticated = await firstValueFrom(
+    authService.ready$.pipe(
+      filter((isReady) => isReady),
+      switchMap(() => authService.isAuthenticated$),
+      take(1),
+    )
+  );
 
   if (!isAuthenticated) {
     authService.login();
